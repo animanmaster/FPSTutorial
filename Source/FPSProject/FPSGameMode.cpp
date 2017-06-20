f88372e9d72b0d5ef6df5c5d0915ec1c177a2059
Malik Ahmed <animanmaster@gmail.com>
2017-06-19 22:48:49 -0400

    Add a Blueprint subclass of the FPSCharacter and assign the generic male mesh to it, then set it as the default pawn in the game mode.

--- Source/FPSProject/FPSGameMode.cpp
+++ Source/FPSProject/FPSGameMode.cpp
@@ -2,13 +2,17 @@
 
 #include "FPSProject.h"
 #include "FPSGameMode.h"
-#include "FPSCharacter.h"
 
 
 AFPSGameMode::AFPSGameMode(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	DefaultPawnClass = AFPSCharacter::StaticClass();
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnObject(TEXT("Pawn'/Game/Blueprints/BP_FPSCharacter.BP_FPSCharacter_C'"));
+	if (PlayerPawnObject.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnObject.Class;
+	}
 }
 
 void AFPSGameMode::StartPlay()
